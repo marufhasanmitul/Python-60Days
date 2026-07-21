2b72b9a4929768a63710b13400e9bd6a44158914
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-21 21:13:53 +0600

    Day 2 Completed

---
+++ Day 2/variable.py
@@ -0,0 +1,36 @@
+name = "Maruf Hasan"
+print(name)
+
+age = 20
+print(age)
+
+#Data Types
+name = "Maruf Hasan"
+age = 20
+height=5.8
+is_student = True
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#User Input
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+
+print("Welcome",name)
+print("Your age is",age)
+
+#Small Project
+
+name = input("নাম: ")
+age = int(input("বয়স: "))
+profession = input("পেশা: ")
+
+print("------")
+print("নাম:", name)
+print("বয়স:", age)
+print("পেশা:", profession)
