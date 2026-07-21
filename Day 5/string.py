438da816f84effd5ff1a5308c64285f5eca92721
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-21 21:52:27 +0600

    Day 5-String ,Concatenation,String Repeat,String Method,f String

---
+++ Day 5/string.py
@@ -0,0 +1,48 @@
+#String (Text) in Python
+
+# f String
+
+name = "Maruf"
+age = 30
+
+print(f"My name is {name}. I am {age} years old.")
+
+#৪. String Method
+
+text="python programming"
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.capitalize())
+print(len(text))
+
+
+
+
+
+
+
+#৩. String Repeat
+print("HI " * 3)
+
+
+
+
+
+
+#২. String যোগ করা (Concatenation)
+
+first= "Maruf"
+last= "Hasan"
+print(first+" "+last)
+
+
+
+
+#১. String তৈরি
+name= "Maruf"
+city= "Rangpur"
+
+print(name)
+print(city)
