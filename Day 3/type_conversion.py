698d4168a81c5c3c8755e094a0c478b454c413ae
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-21 21:21:13 +0600

    Day 3-User Input & Type Conversion

---
+++ Day 3/type_conversion.py
@@ -0,0 +1,23 @@
+age =int(input("Enter your age:"))
+
+print("Your age is",age+5)
+
+
+#Mini Project
+
+name = input("Name: ")
+age = int(input("Age: "))
+cgpa = float(input("CGPA: "))
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+
+
+#Sumision
+a=int(input("Enter a First number:"))
+b=int(input("Enter a Second number:"))
+
+print("Sum: ", a+b)
+print("Difference: ", a-b)
+print("Product: ", a*b)
