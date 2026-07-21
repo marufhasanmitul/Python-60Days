d0eb8cc786c6e50f4ae8fa85985badd1f2056de3
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-21 21:39:59 +0600

    Day 4-User Input & Type Conversion mini calculator BMI calculator Age Calculator

---
+++ Day 4/type_con_mini.py
@@ -0,0 +1,58 @@
+#mini Calculator
+
+number1=int(input("Enter first number: "))
+number2=int(input("Enter second number: "))
+
+print("Sum: ", number1+number2)
+print("Subtract: ", number1-number2)
+print("Multiplication: ", number1*number2)
+print("Divide: ", number1/number2)
+
+
+
+
+
+
+
+#৫. Mini Project – BMI Calculator
+
+weight=float(input("Weight (kg): "))
+height = float(input("Height (meter): "))
+
+bmi=weight/(height ** 2)
+
+print("Your BMI is", bmi)
+
+
+
+
+#৪. Mini Project – Age Calculator
+
+birth_year = int(input("Enter your birth year: "))
+current_year = 2026
+
+age = current_year - birth_year
+
+print("Your current age is", age)
+
+
+
+
+
+#১. input()
+
+name = input("Enter your name: ")
+
+print("Hello",name)
+
+#Number Input
+age = int(input("Enter your age: "))
+
+print("Your age is",age)
+
+#৩. দুইটি সংখ্যা যোগ
+
+num1=int(input("First number: "))
+num2=int(input("Second number: "))
+
+print("Sum: ", num1+num2)
