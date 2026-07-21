f4199aeb62ae08ab7facef7808d21fbf1a8fbce1
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-21 22:06:42 +0600

    Day 6 - Arithmetic Operators,Comparison Operators,Logical Operators,Assignment Operators

---
+++ Day 6/operators.py
@@ -0,0 +1,60 @@
+#৪. Assignment Operators
+a=10
+
+a += 5
+print(a)   # 15
+
+a -= 3
+print(a)   # 12
+
+a *= 2
+print(a)   # 24
+
+
+
+
+
+
+
+
+
+
+#৩. Logical Operators
+
+print(True and False)   # False
+print(True or False)    # True
+print(not True)         # False
+
+print("   ")
+print("   ")
+print("   ")
+print("   ")
+
+
+
+#২. Comparison Operators
+
+x=10
+y=20
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+
+
+
+#১. Arithmetic Operators
+a=10
+b=3
+
+print(a+b)  # যোগ
+print(a-b)  # বিয়োগ
+print(a*b)  # গুণ
+print(a/b)  # ভাগ
+print(a//b) # পূর্ণসংখ্যা ভাগ
+print(a%b)  # ভাগশেষ
+print(a**b) # পাওয়ার
