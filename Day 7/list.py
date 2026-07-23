bc7ef339458e212cecf7aa89239129702b186461
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-23 15:04:44 +0600

    Day 7 -List,append,index,remove,loop print

---
+++ Day 7/list.py
@@ -0,0 +1,32 @@
+
+
+
+student=[]
+student.append("Kamal")
+student.append("Rosalind")
+student.append("Jubayer")
+student.append("Mithon")
+student.append("Jakarta")
+print(student)
+
+
+
+name=["maruf","Hasan","Rahim"]
+print(name[1])
+
+
+
+
+#create a List
+fruits=["apple","banana","mango"]
+print(fruits)
+print(fruits[2])
+print(fruits[-1])
+fruits.append("Lichi")
+print(fruits)
+fruits.remove("apple")
+print(fruits)
+print(len(fruits))
+
+for fruit in fruits:
+    print(fruit)
