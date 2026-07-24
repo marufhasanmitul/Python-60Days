dfe76893b90a61d0e9540458dc0401100abfad58
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2026-07-24 08:43:22 +0600

    Day 9 - set

---
+++ Day 9/set.py
@@ -0,0 +1,17 @@
+fruits = {"Apple", "Banana", "Mango"}
+
+print(fruits)
+
+numbers = {1, 2, 2, 3, 4, 4, 5}
+
+print(numbers)
+
+fruits = {"Apple", "Banana"}
+
+fruits.add("Orange")
+
+print(fruits)
+
+fruits.update(["Mango", "Lichi"])
+
+print(fruits)
